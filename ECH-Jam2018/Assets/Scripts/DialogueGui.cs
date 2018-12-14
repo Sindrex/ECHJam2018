e9dd2ecb9222d92bb9f1a8501e3550343e364c0b
dogiordano <dogiordano@gmail.com>
2018-12-14 23:58:04 +0100

    Add dialogue box animations;

--- ECH-Jam2018/Assets/Scripts/DialogueGui.cs
+++ ECH-Jam2018/Assets/Scripts/DialogueGui.cs
@@ -1,5 +1,6 @@
 ﻿namespace GameJam
 {
+    using System;
     using System.Collections;
     using TMPro;
     using UnityEngine;
@@ -15,6 +16,26 @@
         TextMeshProUGUI m_words;
         [SerializeField]
         GameObject m_endOfLine;
+        [SerializeField]
+        CanvasGroup m_canvas;
+        [SerializeField]
+        float m_duration = 0.7f;
+
+        bool IsAnimating
+        {
+            get { return enabled; }
+            set { enabled = value; }
+        }
+
+        [NonSerialized]
+        float m_startAlpha = 0f;
+        [NonSerialized]
+        float m_targetAlpha = 1f;
+
+        [NonSerialized]
+        float m_currentLength = 0;
+        [NonSerialized]
+        string m_targetText;
 
         public IEnumerator ShowLineAsync(CloseUp closeup, string name, string line)
         {
@@ -23,15 +44,61 @@
 
             m_closeup.CopyFrom(closeup);
             m_name.text = name;
-            m_words.text = line;
-            yield return null;
+
+            m_targetText = line;
+            m_currentLength = 0;
+
+            m_startAlpha = 0f;
+            m_targetAlpha = 1f;
+            IsAnimating = true;
+            while(IsFading || IsWritingLetters)
+            {
+                yield return null;
+            }
             m_endOfLine.SetActive(true);
         }
 
         public IEnumerator HideAsync()
         {
-            yield return null;
+            m_startAlpha = 1f;
+            m_targetAlpha = 0f;
+            IsAnimating = true;
+            while (IsFading)
+            {
+                yield return null;
+            }
             gameObject.SetActive(false);
+        }
+
+        bool IsFading
+        {
+            get { return !Mathf.Approximately(m_canvas.alpha, m_targetAlpha); }
+        }
+        void Fade()
+        {
+            float delta = Time.deltaTime * (m_targetAlpha - m_startAlpha) / m_duration;
+            m_canvas.alpha += delta;
+        }
+        bool IsWritingLetters
+        {
+            get { return (int)m_currentLength != m_targetText.Length; }
+        }
+        void WriteLetters()
+        {
+            float deltaLength = Time.deltaTime * ((float)m_targetText.Length) / m_duration;
+            m_currentLength += deltaLength;
+            m_currentLength = Mathf.Clamp(m_currentLength, 0, m_targetText.Length);
+            m_words.text = m_targetText.Substring(0, (int)m_currentLength);
+
+            if ((int)m_currentLength == m_targetText.Length)
+            {
+                m_currentLength = (int)m_currentLength;
+            }
+        }
+        void Update()
+        {
+            if (IsFading) Fade();
+            if (IsWritingLetters) WriteLetters();
         }
     }
 }
